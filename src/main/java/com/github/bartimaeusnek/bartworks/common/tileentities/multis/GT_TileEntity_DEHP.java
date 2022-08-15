651f95cc70cf06f16874befbfc311defba651e3d
miozune <miozune@gmail.com>
2022-08-15 19:58:49 +0900

    Make descriptions in line with GT
    Former-commit-id: 482b27c858222b62ac87ac25f2a8295ef81642ab

--- src/main/java/com/github/bartimaeusnek/bartworks/common/tileentities/multis/GT_TileEntity_DEHP.java
+++ src/main/java/com/github/bartimaeusnek/bartworks/common/tileentities/multis/GT_TileEntity_DEHP.java
@@ -124,7 +124,7 @@ public class GT_TileEntity_DEHP extends GT_MetaTileEntity_DrillerBase {
         tt.addSeparator()
                 .beginStructureBlock(3, 7, 3, false)
                 .addController("Front bottom")
-                .addStructureInfo(casings + " form the 3x1x3 Base")
+                .addOtherStructurePart(casings, "form the 3x1x3 Base")
                 .addOtherStructurePart(casings, " 1x3x1 pillar above the center of the base (2 minimum total)")
                 .addOtherStructurePart(getFrameMaterial().mName + " Frame Boxes", "Each pillar's side and 1x3x1 on top")
                 .addEnergyHatch(VN[getMinTier()] + "+, Any base casing")
