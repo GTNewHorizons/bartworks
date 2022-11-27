21e89f2497d854faeaac8fb8b751e855b33ca7be
miozune <miozune@gmail.com>
2022-11-27 15:34:34 +0900

    Windmill
    Former-commit-id: 21c1b173f30a22befa0d31050c7c3cb5c0bf826c

--- src/main/java/com/github/bartimaeusnek/bartworks/API/modularUI/BW_UITextures.java
+++ src/main/java/com/github/bartimaeusnek/bartworks/API/modularUI/BW_UITextures.java
@@ -2,9 +2,17 @@ package com.github.bartimaeusnek.bartworks.API.modularUI;
 
 import static com.github.bartimaeusnek.bartworks.MainMod.MOD_ID;
 
+import com.gtnewhorizons.modularui.api.drawable.AdaptableUITexture;
 import com.gtnewhorizons.modularui.api.drawable.UITexture;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class BW_UITextures {
+
+    public static final AdaptableUITexture BACKGROUND_BROWN =
+            AdaptableUITexture.of(MOD_ID, "GUI/background/brown", 176, 166, 3);
+
+    public static final AdaptableUITexture SLOT_BROWN = AdaptableUITexture.of(MOD_ID, "GUI/slot/brown", 18, 18, 1);
 
     public static final UITexture OVERLAY_SLOT_DISH = UITexture.fullImage(MOD_ID, "GUI/overlay_slot/dish");
     public static final UITexture OVERLAY_SLOT_DNA_FLASK = UITexture.fullImage(MOD_ID, "GUI/overlay_slot/dna_flask");
@@ -21,4 +29,17 @@ public class BW_UITextures {
     public static final UITexture PICTURE_DECAY_TIME_INSIDE =
             UITexture.fullImage(MOD_ID, "GUI/picture/decay_time_inside");
     public static final UITexture PICTURE_RADIATION = UITexture.fullImage(MOD_ID, "GUI/picture/radiation");
+    public static final UITexture PICTURE_WINDMILL_EMPTY = UITexture.fullImage(MOD_ID, "GUI/picture/windmill_empty");
+    public static final UITexture[] PICTURE_WINDMILL_ROTATING = IntStream.range(0, 4)
+            .mapToObj(i ->
+                    UITexture.partly(MOD_ID, "GUI/picture/windmill_rotating", 32, 128, 0, i * 32, 32, (i + 1) * 32))
+            .collect(Collectors.toList())
+            .toArray(new UITexture[0]);
+
+    public static final AdaptableUITexture TAB_TITLE_BROWN =
+            AdaptableUITexture.of(MOD_ID, "GUI/tab/title_brown", 28, 28, 4);
+    public static final AdaptableUITexture TAB_TITLE_DARK_BROWN =
+            AdaptableUITexture.of(MOD_ID, "GUI/tab/title_dark_brown", 28, 28, 4);
+    public static final AdaptableUITexture TAB_TITLE_ANGULAR_BROWN =
+            AdaptableUITexture.of(MOD_ID, "GUI/tab/title_angular_brown", 28, 28, 4);
 }
