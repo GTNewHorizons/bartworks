8f13eb2f279bf388a9e121b16d4484369814b82f
miozune <miozune@gmail.com>
2022-11-27 13:19:08 +0900

    NEI
    Former-commit-id: 0a81d1843acc6776030eea4014f4e3d89680945f

--- src/main/java/com/github/bartimaeusnek/bartworks/API/modularUI/BW_UITextures.java
+++ src/main/java/com/github/bartimaeusnek/bartworks/API/modularUI/BW_UITextures.java
@@ -6,13 +6,19 @@ import com.gtnewhorizons.modularui.api.drawable.UITexture;
 
 public class BW_UITextures {
 
-    public static final UITexture PROGRESSBAR_SIEVERT = UITexture.fullImage(MOD_ID, "GUI/progressbar/sievert.png");
+    public static final UITexture OVERLAY_SLOT_DISH = UITexture.fullImage(MOD_ID, "GUI/overlay_slot/dish");
+    public static final UITexture OVERLAY_SLOT_DNA_FLASK = UITexture.fullImage(MOD_ID, "GUI/overlay_slot/dna_flask");
+    public static final UITexture OVERLAY_SLOT_MODULE = UITexture.fullImage(MOD_ID, "GUI/overlay_slot/module");
+    public static final UITexture OVERLAY_SLOT_ROD = UITexture.fullImage(MOD_ID, "GUI/overlay_slot/rod");
 
-    public static final UITexture PICTURE_BW_LOGO_47X21 = UITexture.fullImage(MOD_ID, "GUI/picture/bw_logo_47x21.png");
+    public static final UITexture PROGRESSBAR_SIEVERT = UITexture.fullImage(MOD_ID, "GUI/progressbar/sievert");
+
+    public static final UITexture PICTURE_BW_LOGO_47X21 = UITexture.fullImage(MOD_ID, "GUI/picture/bw_logo_47x21");
     public static final UITexture PICTURE_SIEVERT_CONTAINER =
-            UITexture.fullImage(MOD_ID, "GUI/picture/sievert_container.png");
+            UITexture.fullImage(MOD_ID, "GUI/picture/sievert_container");
     public static final UITexture PICTURE_DECAY_TIME_CONTAINER =
-            UITexture.fullImage(MOD_ID, "GUI/picture/decay_time_container.png");
+            UITexture.fullImage(MOD_ID, "GUI/picture/decay_time_container");
     public static final UITexture PICTURE_DECAY_TIME_INSIDE =
-            UITexture.fullImage(MOD_ID, "GUI/picture/decay_time_inside.png");
+            UITexture.fullImage(MOD_ID, "GUI/picture/decay_time_inside");
+    public static final UITexture PICTURE_RADIATION = UITexture.fullImage(MOD_ID, "GUI/picture/radiation");
 }
