f50aa37aaff43fbaed42b0570d860c667603454b
kuba6000 <kuba.123123.6000@gmail.com>
2022-12-18 15:44:08 +0100

    Ok
    Former-commit-id: 1123c8250e3337a127a4f03ec55a39581d3aba6d

--- src/main/java/com/github/bartimaeusnek/bartworks/client/renderer/BW_EICPistonVisualizer.java
+++ src/main/java/com/github/bartimaeusnek/bartworks/client/renderer/BW_EICPistonVisualizer.java
@@ -41,14 +41,9 @@ public class BW_EICPistonVisualizer extends EntityFX {
         double f12 = this.prevPosY + (this.posY - this.prevPosY) * (double) p_70539_2_ - interpPosY;
         double f13 = this.prevPosZ + (this.posZ - this.prevPosZ) * (double) p_70539_2_ - interpPosZ;
 
-        // tessellator.draw();
-        // tessellator.startDrawingQuads();
-        // tessellator.setTranslation(f11 - this.posX, f12 - posY, f13 - posZ);
-        tessellator.setColorRGBA(255, 255, 255, 255);
         RenderBlocks.getInstance().blockAccess = this.worldObj;
-        /*
+        tessellator.setTranslation(f11 - this.posX, f12 - this.posY, f13 - this.posZ);
         RenderBlocks.getInstance().setRenderFromInside(false);
-        RenderBlocks.getInstance().setRenderAllFaces(true);
         RenderBlocks.getInstance()
                 .renderBlockUsingTexture(
                         GregTech_API.sBlockMetal5,
@@ -56,22 +51,8 @@ public class BW_EICPistonVisualizer extends EntityFX {
                         (int) this.posY,
                         (int) this.posZ,
                         GregTech_API.sBlockMetal5.getIcon(0, 2));
+        tessellator.setTranslation(0d, 0d, 0d);
 
-         */
-
-        RenderBlocks.getInstance().setRenderFromInside(false);
-        RenderBlocks.getInstance()
-                .renderFaceXPos(GregTech_API.sBlockMetal5, f11, f12, f13, GregTech_API.sBlockMetal5.getIcon(0, 2));
-        RenderBlocks.getInstance()
-                .renderFaceXNeg(GregTech_API.sBlockMetal5, f11, f12, f13, GregTech_API.sBlockMetal5.getIcon(0, 2));
-        RenderBlocks.getInstance()
-                .renderFaceYPos(GregTech_API.sBlockMetal5, f11, f12, f13, GregTech_API.sBlockMetal5.getIcon(0, 2));
-        RenderBlocks.getInstance()
-                .renderFaceYNeg(GregTech_API.sBlockMetal5, f11, f12, f13, GregTech_API.sBlockMetal5.getIcon(0, 2));
-        RenderBlocks.getInstance()
-                .renderFaceZPos(GregTech_API.sBlockMetal5, f11, f12, f13, GregTech_API.sBlockMetal5.getIcon(0, 2));
-        RenderBlocks.getInstance()
-                .renderFaceZNeg(GregTech_API.sBlockMetal5, f11, f12, f13, GregTech_API.sBlockMetal5.getIcon(0, 2));
         GL11.glEnable(GL11.GL_CULL_FACE);
         GL11.glDepthMask(true);
     }
